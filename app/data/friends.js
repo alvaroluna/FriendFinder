5f6aa359b1439741f923d4ae7f7b8aa7c9fd8687
alvaroluna <luna.alvaro3@gmail.com>
2020-04-21 23:41:07 -0700

    Finished friends.js file

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+// Data structure that will store the friends data
+var friendsArray = [{
+        name: 'Luis',
+        photo: 'app/images/luisSiba.jpg',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Julia',
+        photo: 'app/images/juliaShiba.jpg',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Cassandra',
+        photo: 'app/images/cassandraShiba.png',
+        scores: [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Carmela',
+        photo: 'app/images/carmelaShiba.jpg',
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: 'Jade',
+        photo: 'app/images/jadeShiba.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: 'Aggie',
+        photo: 'app/images/aggieShiba.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Mario',
+        photo: 'app/images/marioShiba.jpg',
+        scores: [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4',
+            '4',
+            '3',
+            '2',
+            '3',
+            '3'
+        ]
+    },
+    {
+        name: 'Yesenia',
+        photo: 'app/images/yeseniaShiba',
+        scores: [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Olivia',
+        photo: 'app/images/oliviaShiba.jpg',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'Cinnamon',
+        photo: 'app/images/cinnamonShiba.jpg',
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    }
+];
+console.log(friendsArray.length)
+module.exports = friendsArray;
